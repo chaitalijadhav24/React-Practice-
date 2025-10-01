ae3c31d7198ecfc1759f59ec32212e59da673d86
Chaitali Jadhav <chaitalijadhav0254@gmail.com>
2025-10-01 10:58:01 -0700

    Add files via upload

---
+++ About.js
@@ -0,0 +1,47 @@
+function About(){
+  return (
+    <div>
+      <main class="MainBar">
+            <div>
+                <h3>Enriching and Fun Early Years <br></br>
+                    Matter Preschool Curriculum</h3>
+                    <p> Our curriculum focuses on the holistic development of the child and is suitably
+                        mapped to age-appropriate objectives and outcomes. Based on Dr. Howard
+                        Gardner’s Theory of Multiple Intelligences and Experiential Learning, the
+                        KLAY curriculum empowers the child to be able to establish a routine, follow
+                        directions, make friends and work independently.</p>
+            </div>
+            <div class="women">
+                <img src="https://i.pinimg.com/736x/f1/48/78/f14878d2783a76dc7a8124f154768230.jpg" height="200px" width="200px"></img>
+            </div>
+        </main>
+
+        <main class="Container">
+            <div class="kid">
+                <img src="https://thumbs.dreamstime.com/b/cartoon-children-playing-hopscotch-together-outdoor-games-kids-having-fun-illustration-three-cute-enjoying-game-376261591.jpg" height="250px" width="250px"></img>
+            </div>
+            <div>
+                <h3>Socio-emotional & Physical<br></br>
+                    Development</h3>
+                    <p>A multi-sensory environment that includes group activities, role plays, story time and drama that have been
+                        curated to meet the child’s social, emotional, physical and emotional milestones. We make them ‘Big School
+                        Ready’!</p>
+            </div>
+        </main>
+
+        <main class="para">
+            <div>
+                <h3> Milestones Tracking and<br></br>
+                    Regular Assessments</h3>
+                    <p> 180 degree evaluation, milestone tracking and regular assessments every
+                        term, to let you know of your child’s progress. We also help counsel with
+                        strategies, should a child require a more focused stimulation</p>
+            </div>
+            <div class="girl">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLXadnd_VeofyD0h2gvUey8EiY0LwtgQtnfg&s" height="200px" width="200px"></img>
+            </div>
+        </main>
+    </div>
+  )
+}
+export default About;
