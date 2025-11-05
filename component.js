e3ddb8df09c340a27d8e63bb03236af5046390a5
Chaitali Jadhav <chaitalijadhav0254@gmail.com>
2025-11-05 03:49:56 -0800

    Functional and class components

---
+++ component.js
@@ -0,0 +1,20 @@
+//  function App(){
+//    return (
+//      <div>
+//        <h1>Functional Component</h1>
+//      </div>
+//    )
+//  }
+//  export default App;
+
+ import React from "react";
+ class App extends React.Component {
+   render() {
+     return (
+       <div>
+         <h1>Hello Class Component</h1>
+       </div>
+     );
+   }
+ }
+ export default App;
