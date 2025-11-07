c19c4cd3dd4807c24095aed0a36a91685646d3f8
Chaitali Jadhav <chaitalijadhav0254@gmail.com>
2025-11-07 01:34:56 -0800

    Contact page

---
+++ Contact.js
@@ -0,0 +1,31 @@
+function Contact() {
+  return (
+    <div>
+       <main class="Contact">
+            <div>
+                <h3>Full Stack Developer Training & Placements</h3><hr></hr>
+                <p> To be an effective full-stack developer, you should acquire and continue upgrading your
+                    knowledge of applicable programming language skills. Consider taking courses in relevant
+                    languages—like Python, HTML, CSS, and Javascript.<br></br>
+                    To be an effective full-stack developer, you should acquire and continue upgrading your
+                    knowledge of applicable programming language skills. Consider taking courses in relevant
+                    languages—like Python, HTML, CSS, and Javascript</p><hr></hr>
+                    <button class="btn1">Learn More</button>
+            </div>
+            <div class="form">
+            <form>
+                <h2> Fill & Get upto<br></br>
+                    25% OFF in<br></br>
+                    Courese Fees</h2>
+                <input type="text" placeholder="Name*" required></input><br></br>
+                <input type="number" placeholder="10 Digit Mobile Number*" required></input><br></br>
+                <input type="emailid" placeholder="Email*" required></input><br></br>
+                <input type="text" placeholder="Message"></input><br></br>
+                <button class="btn1">I am Intrested, Tell me more.. </button>
+            </form>
+            </div>
+        </main>
+    </div>
+  )
+}
+export default Contact;
