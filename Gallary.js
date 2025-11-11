e0e96b4480de5239e33d8ab79234fe07947f20a5
Chaitali Jadhav <chaitalijadhav0254@gmail.com>
2025-11-11 10:21:41 -0800

    Add files via upload

---
+++ Gallary.js
@@ -0,0 +1,37 @@
+function Gallery() {
+  return (
+    <div>
+      <main class="first">
+            <div>
+                <img src="https://www.parents.com/thmb/595334Wl0HQnRAxfMv6Fy7bf9ME=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/shutterstock_172261862-2--d36510500ca044e1bd9bb51f798b3141.jpg" height="250px" width="250px"></img>
+            </div>
+            <div>
+                <img src="https://cdn.apartmenttherapy.info/image/upload/f_auto,q_auto:eco,c_fill,g_auto,w_1500,ar_3:2/stock%2FGettyImages-473032112" height="250px" width="250px"></img>
+            </div>
+            <div>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8txkpGmdfDTFSEB6vKY68R_OHf69htqRvzkDAUuv--fH_PYZU1QzUse5hdxv83iEvDsg&usqp=CAU" height="250px" width="250px"></img>
+            </div>
+            <div>
+                <img src="https://thumbs.dreamstime.com/b/happy-kids-preschool-kindergarten-children-international-teacher-219273102.jpg" height="250px" width="250px"></img>
+            </div>
+            </main>
+            <main class="second">
+            <div>
+                <img src="https://thumbs.dreamstime.com/z/children-school-making-something-out-colored-paper-group-table-primary-craft-lesson-primary-77692920.jpg" height="250px" width="250px"></img>
+            </div>
+            <div>
+                <img src="https://thumbs.dreamstime.com/z/education-children-painting-teacher-class-art-creative-talent-motor-skill-development-preschool-learning-students-379331456.jpg" height="250px" width="250px"></img>
+            </div>
+            <div>
+                <img src="https://img.freepik.com/free-photo/children-playing-grass_1098-504.jpg" height="250px" width="250px"></img>
+            </div>
+            <div>
+                <img src="https://media.istockphoto.com/id/533745979/photo/playful-group-of-kids-in-the-park.jpg?s=170667a&w=0&k=20&c=k-08tpzCC30hQMT3L57bhy5tpjGFiOaUL4l2gwTtTvo=" height="250px" width="250px"></img>
+            </div>
+            
+        </main>
+        <button class="btn">Get to know us better!</button>
+    </div>
+  );
+}
+export default Gallery;
