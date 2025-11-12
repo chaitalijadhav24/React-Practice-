728a92710ec3a16b76163858168abbc466dff928
Chaitali Jadhav <chaitalijadhav0254@gmail.com>
2025-11-12 01:55:58 -0800

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,25 @@
+function Home(){
+  return (
+    <div>
+      <main class="mainbar">
+          <div class="content">
+            <h3>Kids Learning Center</h3><hr></hr>
+            <h2>New Approach to <br></br>
+                    Kids Education</h2>
+                    <p> Make education possible for every child. Join hands with an
+                        NGO and serve the poor. Donate for education and help
+                        children attain age-appropriate learning. Make education
+                        possible for every child. Join hands with an NGO and serve
+                        the poor. Donate for education and help children attain age
+                        appropriate learning</p><hr></hr>
+                        <button class="btn1">Learn More</button>
+          </div>
+          <div class="kids">
+                <img src="https://www.parents.com/thmb/595334Wl0HQnRAxfMv6Fy7bf9ME=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/shutterstock_172261862-2--d36510500ca044e1bd9bb51f798b3141.jpg" height="300px" width="300px">
+                </img>
+            </div>
+        </main>
+    </div>
+  )
+}
+export default Home;
