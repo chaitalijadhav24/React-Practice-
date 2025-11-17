ccfeb80a341eb8764991d109458976a3625834a6
Chaitali Jadhav <chaitalijadhav0254@gmail.com>
2025-11-16 22:35:29 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+// import App from "./App.js";
+// import ReactDOM from "react-dom/client";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <App />
+// );
+
+// import App from "./App.js";
+
+// import App from "./state.js"
+
+
+// import App1 from "./context.js";
+// import App from "./ref.js"
+
+// import App from "./Frm_validation.js"
+
+// import App from "./form.js"
+
+// import App from "./Comlifecyc.js";
+
+// import App from "./props.js"
+
+// import App from "./Route.js"
+
+// import store from "./store.js";
+
+//  import App from "./redux.js"
+
+// import App from "./Composition.js"
+
+import App from "./component.js"
+
+// import App from "./Api.js"
+
+import ReactDOM from "react-dom/client";
+
+//  import { Provider } from "react-redux";
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    //  <Provider store={store}>
+    //      <App/>
+    //  </Provider>
+
+    <App/>
+);
