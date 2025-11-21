a7b065ff3664339c69e62c2eb18a6df56a4bc1e3
Chaitali Jadhav <chaitalijadhav0254@gmail.com>
2025-11-20 22:26:19 -0800

    Add files via upload

---
+++ Page.js
@@ -0,0 +1,34 @@
+function Page(){
+    return(
+        <div>
+             <main class="page"><br></br><hr></hr>
+            <div id="mainbar">
+            <h4>Front end!</h4>
+            <h4>Back end!</h4>
+            <h4>Data base!</h4>
+            <h4>Career Path!</h4>
+            </div><hr></hr>
+       
+        <div>
+        <h2>Benefits of Working at KLAY</h2><hr></hr>
+            <p class="para"> At KLAY, We believe, Happy Employees = Happy Children = Happy Customers! </p> 
+                <p>Therefore, we provide benefits beyond the basic necessities like medical insurance & flexibility that support a balanced
+                life</p><hr></hr>
+        </div>
+        <div class="section">
+            <p>Staff Child Care </p>
+            <p>Medical Insurance/ESI</p>
+            <p>Provident Fund</p>
+            <p>Annual Bonus</p>
+            <p> Attendance</p>
+        </div><hr></hr>
+        <div>
+            <p> In order to enable employees to bring their children to work and provide them with world class education, we have an employee-child education policy in
+                place. Employees with young children who fit into the age group and schooling programmes offered in the Centre they are working at, can enrol their
+                child in to their age specific programmes. The fee for the schooling/day care program will be subsidised</p>
+        </div>
+         </main>
+        </div>
+    );
+}
+export default Page;
